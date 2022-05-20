984e3f70c8dcc665d235802715903f882a1cf7e3
ts5506r <thomasstoyles1@gmail.com>
2022-05-20 11:02:42 +0100

    TS: Changes

--- questions/example.py
+++ questions/example.py
@@ -14,4 +14,9 @@
 	# What was the name of the function we have seen which changes the case of a string?  Use your CLI to access the Python documentation and get help(str).
     
 def endsPy(input):
-	return False
+	if input.lower().endswith('py'):
+		return True
+	else:
+		return False
+
+		
